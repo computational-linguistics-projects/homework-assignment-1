2ab0675ef69665059a73873e011e947bf23d1561
ruano-r <ritaveigamacedo@gmail.com>
2025-05-14 12:06:46 +0200

    Top comment file

--- corpusreader.py
+++ corpusreader.py
@@ -1,1 +1,4 @@
+"""This file defines the class CorpusReader. It provides an initializer that accepts any directory path name for the corpus it will read.
+It has the method sents() that returns the text of the corpus as a list of tokenized sentences, i.e., the corpus becomes a list of lists of tokens."""
+
 
