326f3ba8a345ff4d441d7bb2f24829f9e5269d04
ruano-r <ritaveigamacedo@gmail.com>
2025-05-17 15:15:30 +0200

    last attempt for now

--- model.py
+++ model.py
@@ -11,20 +11,20 @@ import re
 
 def generate_sentence(ngram_model=NgramModel):
     sentence = ['<s>']
-    for _ in range(1, 40):
-        newword = ngram_model.choose_successor(sentence)
-        word = newword[0]
-        if re.search('</s>', word):
-            sentence.append('</s>')
-            break
-        else:
+    newword = ngram_model.choose_successor(sentence)
+    word = newword[0]
+    if re.search('</s>', word):
+        sentence.append('</s>')
+        return sentence
+    else:
+        for _ in range (1,50):       
             sentence.append(word)
-    return sentence
+       
 
     
     
 
-__name__=="__main__"
+#__name__=="__main__"
 testcorpus= CorpusReader("C:/Users/ritav/OneDrive - Universiteit Utrecht/A computational linguistics/train")
 sentences = testcorpus.sents()  # a list of lists of tokens
 test=NgramModel(sentences,2) 
